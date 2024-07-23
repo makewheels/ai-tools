6c64349257454cd1e8ca2c88fc84636af5662c06
miuser <miuser@email.com>
2024-07-24 00:46:52 +0800

    init

---
+++ src/main/java/com/github/makewheels/ai_tools/AiToolsApplication.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.ai_tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiToolsApplication.class, args);
+	}
+
+}
