988c96f1e3116999257689fff31a1cc3034263d7
miuser <miuser@email.com>
2024-07-27 00:33:52 +0800

    尝试修复redis链接问题

--- src/main/java/com/github/makewheels/aitools/system/health/HealthCheckController.java
+++ src/main/java/com/github/makewheels/aitools/system/health/HealthCheckController.java
@@ -4,6 +4,7 @@ import cn.hutool.core.date.DateUtil;
 import com.github.makewheels.aitools.system.context.RequestUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +13,13 @@ import java.util.Date;
 @RestController
 @Slf4j
 public class HealthCheckController {
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
     @GetMapping("healthCheck")
     public String healthCheck() {
         HttpServletRequest request = RequestUtil.getRequest();
+        log.info("redisHost = " + redisHost);
         log.info("healthCheck---" + DateUtil.formatDateTime(new Date()) + "-" + request.getRequestURL());
         return "ok " + System.currentTimeMillis();
     }
