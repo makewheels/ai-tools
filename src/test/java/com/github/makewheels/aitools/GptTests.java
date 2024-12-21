aa109dda1a7fcac7ac36e37a0b0afd99ea41b103
miuser <miuser@email.com>
2024-12-22 00:46:29 +0800

    版本升级

---
+++ src/test/java/com/github/makewheels/aitools/GptTests.java
@@ -0,0 +1,11 @@
+package com.github.makewheels.aitools;
+
+import org.junit.jupiter.api.Test;
+
+public class GptTests {
+
+    @Test
+    public void test() {
+        System.out.println("fewf");
+    }
+}
